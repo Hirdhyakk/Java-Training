1e0b3993ad3bd5c4182ad80b2977a13c073efa57
Hirdhya Khanna <108194883+Hirdhyakk@users.noreply.github.com>
2023-08-11 23:09:37 +0530

    Add files via upload

---
+++ Day 18/Time.java
@@ -0,0 +1,18 @@
+class Time
+{
+    public static void main(String[] args)
+    {
+        for(int i=1; i<=10; i++)
+        {
+            System.out.println(i);
+            try
+            {
+                Thread.sleep(1000);
+            }
+            catch(Exception e)
+            {
+                System.out.println(e);
+            }
+        }
+    }
+}
